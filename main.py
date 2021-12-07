690f9d80d651ee6a945b4d2de0ca39b1c065d8fc
Saimon911 <Saimon_911@mail.ru>
2021-12-07 22:33:02 +0500

    main functions

--- main.py
+++ main.py
@@ -1,8 +1,30 @@
-class PhoneContact:
-    def __init__(self, name, contact_number, note):
+class UserContact:
+    def __init__(self, name, contact_number, status):
         self.name = str(name)
         self.contact_number = str(contact_number)
-        self.note = str(note)
+        self.status = str(status)
 
-    def string_information(self, name, contact_number, note):
-        information = f'Name:{name}; Phone:{contact_number}; Note:{note}'
+    def string_information(self):
+        information_dict = {"Name": self.name, "Contact number": self.contact_number, "Status": self.status}
+        return information_dict
+
+
+class UserBook:
+    def __init__(self):
+        self.list_contact = []
+
+    def add_contact(self, user):
+        self.list_contact.append(user)
+        return self.list_contact
+
+    def get_contact_by_name(self, name):
+        for i in self.list_contact:
+            if i['Name'] == name:
+                return i
+
+
+
+
+
+
+
